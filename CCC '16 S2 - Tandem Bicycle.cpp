042395773a5054eac7fb2767a827456ed3381dd5
Unknown <ankitp5469@gmail.com>
2017-11-04 17:47:03 -0400

    Update

---
+++ CCC '16 S2 - Tandem Bicycle.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// A utility function to get maximum value in arr[]
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+ 
+// A function to do counting sort of arr[] according to
+// the digit represented by exp.
+void countSort(int arr[], int n, int exp)
+{
+    int output[n]; // output array
+    int i, count[10] = {0};
+ 
+    // Store count of occurrences in count[]
+    for (i = 0; i < n; i++)
+        count[ (arr[i]/exp)%10 ]++;
+ 
+    // Change count[i] so that count[i] now contains actual
+    //  position of this digit in output[]
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+ 
+    // Build the output array
+    for (i = n - 1; i >= 0; i--)
+    {
+        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+        count[ (arr[i]/exp)%10 ]--;
+    }
+ 
+    // Copy the output array to arr[], so that arr[] now
+    // contains sorted numbers according to current digit
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+ 
+// The main function to that sorts arr[] of size n using 
+// Radix Sort
+void radixsort(int arr[], int n)
+{
+    // Find the maximum number to know number of digits
+    int m = getMax(arr, n);
+ 
+    // Do counting sort for every digit. Note that instead
+    // of passing digit number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (int exp = 1; m/exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+
+int max(int a, int b) {
+    if(a > b) {
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+
+int main() {
+    
+    // type of question
+    int type;
+    cin >> type;
+    
+    // number of citizens in both countries
+    int n;
+    cin >> n;
+    
+    //speeds of Dmojistan
+    int speedsD[n];
+    for(int i = 0; i < n; i++) {
+        cin >> speedsD[i];
+      //  teams[i][0] = speedsD[i];
+    }
+    
+    //speeds of Pegland    
+    int speedsP[n];
+    for(int j = 0; j < n; j++) {
+        cin >> speedsP[j];
+       // teams[j][1] = speedsP[i]; 
+    }
+    
+    radixsort(speedsD,n);
+    radixsort(speedsP,n);
+    
+    int totalspeed = 0;
+    if(type == 1) {
+        for(int i = 0; i < n; i++) {
+             totalspeed += max(speedsP[i], speedsD[i]);
+        }
+    }
+    
+    else if(type == 2) {
+        for(int i = 0; i < n; i++) {
+             totalspeed += max(speedsP[n-1-i], speedsD[i]);
+        }
+    }
+    
+    cout << totalspeed;
+    
+    return 0;
+}
