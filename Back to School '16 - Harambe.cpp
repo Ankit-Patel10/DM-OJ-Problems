18944686b951e9ec8b12056385acce19bb3b22dd
Unknown <ankitp5469@gmail.com>
2017-11-04 22:22:53 -0500

    Update

---
+++ Back to School '16 - Harambe.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string k;
+    getline(cin, k);
+    int uppercase = 0;
+    int lowercase = 0;
+    for(int i = 0; i < k.length(); i++) {
+        if(k.at(i) >= 65 && k.at(i) <= 90) {
+            uppercase++;
+        }
+        else if(k.at(i) >= 97 && k.at(i) <= 122) {
+            lowercase++;
+        }
+    }
+    if(uppercase>lowercase) {
+        for(int i = 0; i < k.length(); i++) {
+            if(k.at(i) >= 97 && k.at(i) <= 122) {
+            k.at(i) -= 32;
+        }
+        }
+    }
+        
+    else if(uppercase<lowercase) {
+        for(int i = 0; i < k.length(); i++) {
+            if(k.at(i) >= 65 && k.at(i) <= 90) {
+            k.at(i) += 32;
+        }
+        }
+    }
+    else {
+    
+        cout << k;
+        return 0;
+    }
+    cout << k;
+    
+    return 0;
+}
