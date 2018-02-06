156cfb8ea7aaa21a5082afa8bdeba2c52c0cd88d
Ankit Patel <ankitp5469@gmail.com>
2018-02-06 11:16:17 -0500

    update

---
+++ CCC '12 S1 - Don't Pass me the ball.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int calcperm(int a) {
+  int sum = 0;
+  for(int first = 1; first <= a-3; first++) {
+    for(int second = first + 1; second <= a-2; second++) {
+      for(int third = second + 1; third<=a-1; third++) {
+        sum++;
+      }
+    }
+  }
+  return sum;
+}
+
+
+int main() {
+    int j;
+    cin >> j;
+    cout << calcperm(j) << endl;
+    return 0;
+}
