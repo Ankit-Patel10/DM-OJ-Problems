6c76a8b8ee911eb6a2e69927a351ee8c73b4fa23
Unknown <ankitp5469@gmail.com>
2018-04-27 10:55:13 -0400

    Commit

--- CCC '15 J5 - PI-Day.cpp
+++ CCC '15 J5 - PI-Day.cpp
@@ -6,6 +6,35 @@ int main() {
   int numOfPies;
   cin >> numofPies;
 
-  int k;
-  cin >> k;
-}
+  int numOfPeople;
+  cin >> numOfPeople;
+
+  if(numOfPeople == numOfPies) {
+    cout << "1" << endl;
+    return 0;
+  }
+  if(numOfPeople == 1) {
+    cout << "1" << endl;
+    return 0;
+  }
+
+  int pies[numOfPeople+1];
+  pies[0] = 0;
+  int currentPies = numOfPies;
+  for(int i = 0; i < numOfPeople; i++) {
+    pies[i] = 0;
+  }
+  pies[numOfPeople] = numOfPies;
+
+  int numOfWays = 0;
+  numOfWays++;
+
+
+  while(pies[i] != )
+      if(pies[i] > (pies[j]+1)) {
+        pies[i]--;
+        pies[i-1]++;
+        numOfWays++;
+      }
+
+  }
