6cebdc9eed1014f40ca0488a705e12d5cbc90b0e
Unknown <ankitp5469@gmail.com>
2018-04-27 02:32:35 -0400

    Commit

---
+++ CCC '15 J3 - Rovarspraket.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  unordered_map<char,int> placement;
+  placement['a'] = 1;
+  placement['b'] = 2;
+  placement['c'] = 3;
+  placement['c'] = 3;
+  placement['c'] = 3;
+  placement['c'] = 3;
+  placement['c'] = 3;
+  placement['c'] = 3;
+  placement['c'] = 3;
+  placement['c'] = 3;
+  placement['c'] = 3;
+  placement['c'] = 3;
+  placement['c'] = 3;
+  placement['c'] = 3;
+}
