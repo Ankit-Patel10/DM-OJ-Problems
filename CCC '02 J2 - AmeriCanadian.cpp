714a360d302e717304ac0a3805139d7df582bc77
Unknown <ankitp5469@gmail.com>
2018-04-27 01:56:26 -0400

    Commit

--- CCC '02 J2 - AmeriCanadian.cpp
+++ CCC '02 J2 - AmeriCanadian.cpp
@@ -6,7 +6,14 @@ int main() {
   string k;
   while(cin >> k) {
     if(k == "quit!") return 0;
-    
+    if(k.length() <= 4) cout << k << endl;
+    else {
+      for(int i = k.length()-2; i < k.length()-1; i++) {
+      if(k[i-1] != 'a' && k[i-1] != 'e' && k[i-1] != 'i' && k[i-1] != 'o' && k[i-1] != 'u' && k[i-1] != 'y' && k[i] == 'o' && k[i+1] == 'r') {
+        k.insert(i+1, "u");
+      }
+    }
   }
-
+    cout << k << endl;
+  }
 }
