f8682f5572963d81ebf61425cfe3d7dda1593b20
Unknown <ankitp5469@gmail.com>
2018-04-27 02:05:21 -0400

    Commit

--- CCC '16 J2 - Magic Squares.cpp
+++ CCC '16 J2 - Magic Squares.cpp
@@ -3,4 +3,17 @@
 using namespace std;
 
 int main() {
+  int sum[8];
+  for(int i = 0 ; i < 8; i++) {
+    sum[i] = 0;
+  }
+  int array[4][4];
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 4; j++) {
+      cin >> array[i][j];
+      sum[i] += array[i][j];
+      sum[i+4] += array[j][i];
+    }
+  }
+  
 }
