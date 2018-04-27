4aa0d89613d1619d85da3a3b6272eb63ff9ba450
Unknown <ankitp5469@gmail.com>
2018-04-27 03:09:19 -0400

    Commit

--- CCC '15 J3 - Rovarspraket.cpp
+++ CCC '15 J3 - Rovarspraket.cpp
@@ -5,9 +5,50 @@ using namespace std;
 string min(int a, int e, int i, int o, int u) {
   if(a <= e && a <= i && a <= o && a <= u) return "a";
   if(e <= a && e <= i && e <= o && e <= u) return "e";
-  if(e <= a && e <= i && e <= o && e <= u) return "i";
-  if(e <= a && e <= i && e <= o && e <= u) return "o";
-  if(e <= a && e <= i && e <= o && e <= u) return "u";
+  if(i <= a && i <= e && i <= o && i <= u) return "i";
+  if(o <= a && o <= e && o <= i && o <= u) return "o";
+  if(u <= a && u <= e && u <= i && u <= o) return "u";
+}
+
+string nextConsonant(string s, int i, unordered_map<char,int> placement) {
+  unordered_map<int,char> Consonant;
+  Consonant[1] = 'a';
+  Consonant[2] = 'b';
+  Consonant[3] = 'c';
+  Consonant[4] = 'd';
+  Consonant[5] = 'e';
+  Consonant[6] = 'f';
+  Consonant[7] = 'g';
+  Consonant[8] = 'h';
+  Consonant[9] = 'i';
+  Consonant[10] = 'j';
+  Consonant[11] = 'k';
+  Consonant[12] = 'l';
+  Consonant[13] = 'm';
+  Consonant[14] = 'n';
+  Consonant[15] = 'o';
+  Consonant[16] = 'p';
+  Consonant[17] = 'q';
+  Consonant[18] = 'r';
+  Consonant[19] = 's';
+  Consonant[20] = 't';
+  Consonant[21] = 'u';
+  Consonant[22] = 'v';
+  Consonant[23] = 'w';
+  Consonant[24] = 'x';
+  Consonant[25] = 'y';
+  Consonant[26] = 'z';
+
+  char k = s[i];
+  if(k == 'z') return "z";
+  int location = placement[k];
+  for(int i = location; i < 27; i++) {
+    if (Consonant[i] != 'a' && Consonant[i] != 'e' && Consonant[i] != 'i' && Consonant[i] != 'o' && Consonant[i] != 'u') {
+      string temp;
+      temp[0] = Consonant[i];
+      return temp;
+    }
+  }
 }
 
 int main() {
@@ -50,8 +91,10 @@ int main() {
       diffi = abs(placement[s[i]] - placement['i']);
       diffo = abs(placement[s[i]] - placement['o']);
       diffu = abs(placement[s[i]] - placement['u']);
-      s.insert(i+1,"")
+      s.insert(i+1,min(diffa,diffe,diffi,diffo,diffu));
+      s.insert(i+2, nextConsonant(s, i, placement));
     }
   }
+  cout << s << endl;
 
 }
