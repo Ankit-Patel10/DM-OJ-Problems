d795794dcf47448327b5820cce489f32cf226eaa
Unknown <ankitp5469@gmail.com>
2018-04-28 17:23:29 -0400

    commit

--- CCC '12 S5 - Mouse Journey.cpp
+++ CCC '12 S5 - Mouse Journey.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 int main() {
-  
+  int R;
+  int C;
+  cin >> R;
+  cin >> C;
+  int BrotherLocation[0][0];
+  BroterLocation = {[R],[C]};
+
+  int numOfCats;
+  cin >> numOfCats;
+
 }
