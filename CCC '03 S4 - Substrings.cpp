0105e316d5121484432a6bdbbc03d6fa8f23e23e
Unknown <ankitp5469@gmail.com>
2018-04-29 14:07:07 -0400

    commit

--- CCC '03 S4 - Substrings.cpp
+++ CCC '03 S4 - Substrings.cpp
@@ -3,5 +3,21 @@
 using namespace std;
 
 int main() {
-  
+  int n;
+  cin >> n;
+  string k;
+  for(int i = 0; i < n; i++) {
+    int ways = 0;
+    cin >> k;
+    unordered_map<string,int> values;
+    for(int j = 0; j < k.length(); k++) {
+      for(int l = 0; l <= j; l++) {
+        if(values.find(k.substr(l,j+1)) == values.end()) {
+          values[k.substr(l,j+1)] = 1;
+          ways++;
+        }
+      }
+    }
+    cout << ways << endl;
+  }
 }
